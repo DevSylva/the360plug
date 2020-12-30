145b4f624074e9431e2b89b63e9505fa15a723d8
Ejike Sylva <sylvaejike@gmail.com>
2020-12-30 18:51:00 +0100

    first commit

--- cruise/urls.py
+++ cruise/urls.py
@@ -4,8 +4,8 @@ from .views import index, services, about, contact, sent
 
 urlpatterns = [
     path('', index, name="index"),
-    path('services/', services, name="services"),
-    path('about/', about, name="about"),
+    path('about/', services, name="services"),
+    path('services/', about, name="about"),
     path('contact/', contact, name="contact"),
     path('messagesent/', sent, name="messagesent"),
 ]
